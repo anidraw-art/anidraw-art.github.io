91e68cbcc82f09516c00b922b4c9fcb8614b0295
Oeponn <tigershi0110@gmail.com>
2024-03-24 17:39:22 -0400

    Only show gradients on desktop

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -1,3 +1,2 @@
 export {default as Card} from './Card';
-export {default as MinimalCard} from './MinimalCard';
 export {default as Header} from './Header';
--- src/components/pages/Test/index.js
+++ src/components/pages/Test/index.js
@@ -1,63 +1,9 @@
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {EffectCoverflow, Pagination, Navigation} from 'swiper/modules';
-
-import {MinimalCard} from '../../global';
-import members from './assets/members';
-
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import './swiper.scss';
 import styles from './styles.module.scss';
 
 function App() {
   return (
     <div className={styles.homeContainer}>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: -5,
-          stretch: 0,
-          depth: 150,
-          // modifier: 2.5,
-          modifier: 0.5,
-          // modifier: 0.001,
-        }}
-        pagination={{el: '.swiperPagination', clickable: true}}
-        navigation={{
-          nextEl: '.swiperButtonNext',
-          prevEl: '.swiperButtonPrev',
-          clickable: true,
-        }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
-        className={styles.swiperContainer}
-      >
-        {members.map((member, index) => (
-          <SwiperSlide key={index}>
-            <MinimalCard member={member} />
-          </SwiperSlide>
-        ))}
-
-        <div className="sliderContoller">
-
-          <div className="swiperButtonPrev sliderButton">
-            <div className={styles.arrowContainer}>
-              <div className={styles.arrow}>◀</div>
-            </div>
-          </div>
-
-          <div className="swiperPagination"></div>
-
-          <div className="swiperButtonNext sliderButton">
-            <div className={styles.arrowContainer}>
-              <div className={styles.arrow}>▶</div>
-            </div>
-          </div>
-        </div>
-      </Swiper>
+      Page under construction...
     </div>
   );
 }
