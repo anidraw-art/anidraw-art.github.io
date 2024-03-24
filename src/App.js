f8b0745a215be6e72768d50c2e9e150bc44c5217
Oeponn <tigershi0110@gmail.com>
2024-03-24 16:09:10 -0400

    Adding pictures and icons

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import {useEffect} from 'react';
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import {Header} from './components/global';
-import {Home, Test} from './components/pages';
+import {Header} from 'components/global';
+import {Home, Test} from 'components/pages';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Shree
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 const App = () => {
-  useEffect(() => {
-    console.log('App Loaded');
-  });
-
   return (
     <div
       className="appContainer"
@@ -38,11 +12,10 @@ const App = () => {
       <BrowserRouter>
         <div className="pageContainer">
           <Routes>
-            <Route path='/' element={<Test/>} />
-            <Route path='/test' element={<Home/>} />
-            {/* <Route path="/about" element={<About />} /> */}
-            {/* <Route path="/settings" element={<Settings />} /> */}
-            {/* <Route element={<PageNotFound />} /> */}
+            <Route path='/' element={<Home/>} />
+            <Route path='/test' element={<Test/>} />
+            {/* <Route path='/' element={<Test/>} />
+            <Route path='/test' element={<Home/>} /> */}
           </Routes>
         </div>
         {/* <Header /> */}
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,25 +1,15 @@
-/* eslint-disable camelcase */
 import {Swiper, SwiperSlide} from 'swiper/react';
+import {EffectCoverflow, Pagination, Navigation} from 'swiper/modules';
+
+import {Card} from 'components/global';
+import members from './assets/members';
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-import {useState} from 'react';
-import {EffectCoverflow, Pagination, Navigation} from 'swiper/modules';
-import slide_image_1 from './assets/images/img_1.jpg';
-import slide_image_2 from './assets/images/img_2.jpg';
-import slide_image_3 from './assets/images/img_3.jpg';
-import slide_image_4 from './assets/images/img_4.jpg';
-import slide_image_5 from './assets/images/img_5.jpg';
-import slide_image_6 from './assets/images/img_6.jpg';
-import slide_image_7 from './assets/images/img_7.jpg';
-
-import './index.scss';
+import './swiper.scss';
 import styles from './styles.module.scss';
+
 function App() {
-  const [activeSlide, setActiveSlide] = useState(0);
   return (
     <div className={styles.homeContainer}>
       <Swiper
@@ -29,7 +19,6 @@ function App() {
         loop={true}
         slidesPerView={'auto'}
         coverflowEffect={{
-          // rotate: -10,
           rotate: -5,
           stretch: 0,
           depth: 150,
@@ -37,56 +26,34 @@ function App() {
           modifier: 0.5,
           // modifier: 0.001,
         }}
-        onSlideChange={(swiper) => {
-          console.log('activeSlide', swiper.activeIndex);
-          setActiveSlide(swiper.activeIndex);
-        }}
-        pagination={{el: '.swiper-pagination', clickable: true}}
+        pagination={{el: '.swiperPagination', clickable: true}}
         navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: '.swiperButtonNext',
+          prevEl: '.swiperButtonPrev',
           clickable: true,
         }}
         modules={[EffectCoverflow, Pagination, Navigation]}
         className={styles.swiperContainer}
       >
-        {[
-          slide_image_1,
-          slide_image_2,
-          slide_image_3,
-          slide_image_4,
-          slide_image_5,
-          slide_image_6,
-          slide_image_7,
-        ].map((image, index) => (
+        {members.map((member, index) => (
           <SwiperSlide key={index}>
-            <div
-              className={
-                `${styles.card}
-                 ${index === activeSlide ? styles.activeSlide : ''}`
-              }
-              style={{backgroundImage: `url(${image})`}}
-            >
-              {/* Content */}
-            </div>
+            <Card member={member} />
           </SwiperSlide>
         ))}
 
-        <div className="slider-controller">
+        <div className="sliderContoller">
 
-          <div className="swiper-button-prev slider-arrow">
-            <ion-icon name="arrow-back-outline"></ion-icon>
-            <div className={styles.arrow}>
-              ◀
+          <div className="swiperButtonPrev sliderButton">
+            <div className={styles.arrowContainer}>
+              <div className={styles.arrow}>◀</div>
             </div>
           </div>
 
-          <div className="swiper-pagination"></div>
+          <div className="swiperPagination"></div>
 
-          <div className="swiper-button-next slider-arrow">
-            <ion-icon name="arrow-forward-outline"></ion-icon>
-            <div className={styles.arrow}>
-              ▶
+          <div className="swiperButtonNext sliderButton">
+            <div className={styles.arrowContainer}>
+              <div className={styles.arrow}>▶</div>
             </div>
           </div>
         </div>
