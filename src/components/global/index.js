f7b6b676f52e61db5c78aeef340e0da04e87a4a6
Oeponn <tigershi0110@gmail.com>
2024-03-24 17:20:19 -0400

    Performance test without gradients

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -1,2 +1,3 @@
 export {default as Card} from './Card';
+export {default as MinimalCard} from './MinimalCard';
 export {default as Header} from './Header';
---
+++ src/components/pages/Test/assets/images/index.js
@@ -0,0 +1,23 @@
+export {default as coconaw} from './cocoNa_w.jpg';
+export {default as doyngu} from './doyngu.jpg';
+export {default as elodeas} from './elodeas.jpg';
+export {default as hyavoc} from './hyavoc.jpg';
+export {default as ratchuuu} from './rat_chuuu.jpg';
+export {default as rin} from './Rinne0611.jpg';
+export {default as ruiton} from './luckyeric320.jpg';
+export {default as litemitan} from './litemitan.jpg';
+export {default as loopkey} from './tsTiTi1.jpg';
+export {default as zbknashi} from './ZBK_Nashi.jpg';
+export {default as zeklewa} from './zeklewa.jpg';
+
+export {default as coconawIcon} from './cocoNa_w-icon.jpg';
+export {default as doynguIcon} from './doyngu-icon.jpg';
+export {default as elodeasIcon} from './elodeas-icon.jpg';
+export {default as hyavocIcon} from './hyavoc-icon.jpg';
+export {default as ratchuuuIcon} from './rat_chuuu-icon.jpg';
+export {default as rinIcon} from './Rinne0611-icon.jpg';
+export {default as ruitonIcon} from './luckyeric320-icon.jpg';
+export {default as litemitanIcon} from './litemitan-icon.jpg';
+export {default as loopkeyIcon} from './tsTiTi1-icon.jpg';
+export {default as zbknashiIcon} from './ZBK_Nashi-icon.jpg';
+export {default as zeklewaIcon} from './zeklewa-icon.jpg';
